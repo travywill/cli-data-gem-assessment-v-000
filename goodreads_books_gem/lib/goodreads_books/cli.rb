5337a90ac145a883aca74d05b4400ae0113b147a
Travis Williamson <travywill@gmail.com>
2018-08-16 01:33:56 +0000

    Added menu method to cli.rb

--- goodreads_books_gem/lib/goodreads_books/cli.rb
+++ goodreads_books_gem/lib/goodreads_books/cli.rb
@@ -5,14 +5,20 @@ class GoodReadsBooks::CLI
   def call
     puts "Welcome to the GoodReads Book Finder!"
     list_books
+    menu
   end
 
   def list_books
-    #
     puts <<-DOC
       1.  Book 1 Title by Book 1 Author
       2.  Book 2 Title by Book 2 Author
     DOC
+  end
+
+  def menu
+    puts "Would you like more information about one of these books? (y/n)"
+    input = gets.strip
+    input
   end
 
 end
