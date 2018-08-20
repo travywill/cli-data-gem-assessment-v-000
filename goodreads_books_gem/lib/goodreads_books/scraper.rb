195eab644dc4208e061895f6bf0a393dadef40ef
Travis Williamson <travywill@gmail.com>
2018-08-20 18:46:12 +0000

    Added scrape_book_profile_page class method to Scraper class

--- goodreads_books_gem/lib/goodreads_books/scraper.rb
+++ goodreads_books_gem/lib/goodreads_books/scraper.rb
@@ -1,11 +1,19 @@
 class GoodReadsBooks::Scraper
-  #
+
   def self.scrape_search_results(search_results_url)
     search_results_array = []
 
     html = open(search_results_url)
     doc = Nokogiri::HTML(html)
 
-    search_results_array
+    return search_results_array
   end
+
+  def self.scrape_book_profile_page(profile_url)
+    books_profile_hash = {}
+
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    return books_profile_hash 
 end
