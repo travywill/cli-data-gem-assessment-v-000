077c62ce361c21afa7df1026491917350e0b4eb8
Travis Williamson <travywill@gmail.com>
2018-08-20 19:01:50 +0000

    Added attr_accessors, initialize & self.all methods to Book class

--- goodreads_books_gem/lib/goodreads_books/book.rb
+++ goodreads_books_gem/lib/goodreads_books/book.rb
@@ -1,3 +1,18 @@
 class GoodReadsBooks::Book
   #
+  attr_accessor :title, :authors, :profile_url, :summary
+
+  @@all = []
+
+  def initialize(book_hash)
+    @title = book_hash[:title]
+    @authors = book_hash[:authors]
+    @profile_url = book_hash[:profile_url]
+    @summary = book_hash[:summary]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
