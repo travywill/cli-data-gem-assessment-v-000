b20968cc2572989e3049de9f06f725f92f5f90c3
Travis Williamson <travywill@gmail.com>
2018-08-20 18:20:04 +0000

    Added nokogiri and open-uri dependencies

--- goodreads_books_gem/lib/goodreads_books.rb
+++ goodreads_books_gem/lib/goodreads_books.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require_relative "./goodreads_books/version"
 require_relative './goodreads_books/cli'
 require_relative './goodreads_books/book'
