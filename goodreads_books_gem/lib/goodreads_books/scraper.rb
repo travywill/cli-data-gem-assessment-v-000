78c7f5eb4615013534aee6909cf0af04d2ad3403
Travis Williamson <travywill@gmail.com>
2018-08-20 18:35:08 +0000

    Added scrape_search_results class method to Scraper class

--- goodreads_books_gem/lib/goodreads_books/scraper.rb
+++ goodreads_books_gem/lib/goodreads_books/scraper.rb
@@ -1,3 +1,11 @@
 class GoodReadsBooks::Scraper
   #
+  def self.scrape_search_results(search_results_url)
+    search_results_array = []
+
+    html = open(search_results_url)
+    doc = Nokogiri::HTML(html)
+
+    search_results_array
+  end
 end
