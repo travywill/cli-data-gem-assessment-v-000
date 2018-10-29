1bd527cd867cb9699146ea46bdabb7397eaf7aa2
Travis Williamson <travywill@gmail.com>
2018-10-29 19:16:23 +0000

    Added comments to CLI

--- goodreads_books_gem/lib/goodreads_books/cli.rb
+++ goodreads_books_gem/lib/goodreads_books/cli.rb
@@ -6,43 +6,45 @@ require_relative '../goodreads_books/cli.rb'
 require_relative '../goodreads_books/book.rb'
 require_relative '../goodreads_books/scraper.rb'
 
-class CLI
+class CLI # This class runs the command-line interface for the gem
 
   def call
+    # This method welcomes the user and runs the program
     puts ""
     puts "Welcome to the GoodReads Book Finder!"
     puts ""
-    menu
+    menu # Runs the menu method
     puts ""
   end
 
   def menu
+    # This method allows the user to search for books based on key words and read a summary of any book in the search results
     puts "Please enter your key words to search for a book: "
     query = gets.strip
     puts ""
-    list_books(query)
+    list_books(query) # Runs the list_books method with the user's key words to print a list of books to the screen
     puts ""
     puts "Would you like to read a summary of one of these books? (y/n)"
     input = gets.strip
-    until input == "y" || input == "n"
+    until input == "y" || input == "n" # Makes sure the user enters valid input
       puts "Please answer with y or n: "
       input = gets.strip
     end
-    if input == "y"
+    if input == "y" # Runs the show_summary method if the user indicated wanting to read a summary
       puts ""
       show_summary
-    elsif input == "n"
+    elsif input == "n" # Asks if the user would like to conduct another search if no summary is wanted
       puts ""
       puts "Would you like to conduct a new search? (y/n)"
       input = gets.strip
-      until input == "y" || input == "n"
+      until input == "y" || input == "n" # Makes sure the user enters valid input
         puts "Please answer with y or n: "
         input = gets.strip
       end
-      if input == "y"
+      if input == "y" # Begins a new search by re-running the menu method
         puts ""
         menu
-      elsif input == "n"
+      elsif input == "n" # Exits the program using the goodbye method if the user does not want to conduct another search
         puts ""
         goodbye
       end
@@ -50,15 +52,16 @@ class CLI
   end
 
   def list_books(search_terms)
-    query = search_terms
-    @scraper = Scraper.new
-    @book_list = @scraper.scrape_search_results(query)
-    @book_list.each_with_index do |book, index|
-      if book.authors.length == 1
+    # This method prints a numbered list of books to the screen based on the user's search terms
+    query = search_terms # The user's search terms
+    @scraper = Scraper.new # Creates a new Scraper object
+    @book_list = @scraper.scrape_search_results(query) # Creates an array of Book objects based on user's search terms
+    @book_list.each_with_index do |book, index| # Prints a numbered list of the Books to the screen
+      if book.authors.length == 1 # Formatting for book with one author
         puts "#{index + 1}. #{book.title} by #{book.authors[0]}"
-      elsif book.authors.length == 2
+      elsif book.authors.length == 2 # Formatting for book with two authors
         puts "#{index + 1}. #{book.title} by #{book.authors[0]} and #{book.authors[1]}"
-      else
+      else # Formatting for book with three or more authors
         authors = ""
         i = 0
         while i < book.authors.length - 2
@@ -72,34 +75,33 @@ class CLI
   end
 
   def show_summary
+    # This method prints a summary of a book to the screen based on the user's input
     puts "Enter the number of the book for which you would like a summary: "
     input = gets.strip
     input = input.to_i
-    until input > 0 && input <= @book_list.length
+    until input > 0 && input <= @book_list.length # Makes sure the user enters valid input
       puts "Please answer with a valid number: "
       input = gets.strip
       input = input.to_i
     end
-    #url = @book_list[input -1].profile_url
-    summary = @scraper.scrape_book_profile_page(@book_list[input -1])
-    #@book_list[input -1].summary = summary
+    summary = @scraper.scrape_book_profile_page(@book_list[input -1]) # Gets the book's summary information
     puts ""
-    puts summary #@book_list[input -1].summary
+    puts summary
     puts ""
     puts "Would you like to read a summary of another one of the books from your search results? (y/n)"
     input = gets.strip
-    until input == "y" || input == "n"
+    until input == "y" || input == "n" # Makes sure the user enters valid input
       puts "Please answer with y or n: "
       input = gets.strip
     end
-    if input == "y"
+    if input == "y" # Reprints list of books if user wants to read another summary from that list
       puts ""
       @book_list.each_with_index do |book, index|
-        if book.authors.length == 1
+        if book.authors.length == 1 # Formatting for book with one author
           puts "#{index + 1}. #{book.title} by #{book.authors[0]}"
-        elsif book.authors.length == 2
+        elsif book.authors.length == 2 # Formatting for book with two authors
           puts "#{index + 1}. #{book.title} by #{book.authors[0]} and #{book.authors[1]}"
-        else
+        else # Formatting for book with three or more authors
           authors = ""
           i = 0
           while i < book.authors.length - 2
@@ -111,19 +113,19 @@ class CLI
         end
       end
       puts ""
-      show_summary
-    elsif input == "n"
+      show_summary # Reruns the show_summary method if the user wants to read another summary from the current list of books
+    elsif input == "n" # Allows user to conduct another search for books if no summary is wanted from the current list
       puts ""
       puts "Would you like to conduct a new search? (y/n)"
       input = gets.strip
-      until input == "y" || input == "n"
+      until input == "y" || input == "n" # Makes sure the user enters valid input
         puts "Please answer with y or n: "
         input = gets.strip
       end
-      if input == "y"
+      if input == "y" # Reruns the menu method if the user wants to conduct another search
         puts ""
         menu
-      elsif input == "n"
+      elsif input == "n" # Exits the program via the goodbye method if user does not want to conduct another search
         puts ""
         goodbye
       end
@@ -131,6 +133,7 @@ class CLI
   end
 
   def goodbye
+    # This method thanks the user upon exiting the program
     puts "Thanks for using the GoodReads Book Finder!"
   end
 
